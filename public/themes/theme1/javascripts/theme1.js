de53be71b2af7adcf41cbf7a1d7ebcb0ad7e1576
shubham gupta <shubham.gupta@vinsol.com>
2014-03-19 18:21:10 +0530

    added seed themes

---
+++ public/themes/theme1/javascripts/theme1.js
@@ -0,0 +1,9 @@
+//= require mediaelement_tree
+//= require ../../../assets/ckeditor/ckeditor
+//= require elementToggle
+//= require pagePreview
+//= require preview_generator
+
+$(function() {
+  $('audio, video').mediaelementplayer();
+});
---
+++ public/themes/vinsol_theme/javascripts/vinsol.js
@@ -0,0 +1,11 @@
+//= require mediaelement_tree
+//= require ../../../assets/ckeditor/ckeditor
+//= require custom
+//= require elementToggle
+//= require pagePreview
+//= require preview_generator
+
+$(function() {
+  $('audio, video').mediaelementplayer();
+});
+
