289c67a45ed7841bb65e21b4cc9cda75f7cfc8f1
shubham gupta <shubham.gupta@vinsol.com>
2014-04-04 16:21:11 +0530

    add secret file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChipotleCds::Application.config.secret_key_base = 'ed4e2090fce7b4de17f1061e410ce8c2757cacf126a18928584906fceddfa95aadf06ae52088eaf53ade44cc343ce548276c63b1aaeab6e510845a165c94b517'
